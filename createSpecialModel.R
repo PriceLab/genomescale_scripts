4cadf40188a020b5fdd7b7e028e563bfdcd1c15a
CoryFunk <cfunk@systemsbiology.org>
2017-04-05 12:30:44 -0700

    TReNA function taking footprints as input

---
+++ createSpecialModel.R
@@ -0,0 +1,39 @@
+function(mtx.assay, gene.list, num.cores = NULL,
+                                   extraArgs = list()){
+
+    trena <- TReNA(mtx.assay, solver = "ensemble")
+
+    lapply(dbListConnections(dbDriver(drv="PostgreSQL")), dbDisconnect)
+
+    # Setup the parallel structure with a default of half the cores
+    if(is.null(num.cores)){
+        num.cores <- detectCores()/2}
+    cl <- makeForkCluster(nnodes = num.cores)
+    registerDoParallel(cl)
+
+    full.result.list <- foreach(i = 1:length(names(gene.list)), .packages='TReNA') %dopar% {
+
+        # Designate the target gene and grab the tfs
+        target.gene <- names(gene.list)[[i]]
+
+        # Solve the trena problem using the supplied values and the ensemble solver
+
+        if(!(class(gene.list) == "try-error")){
+            if(length(gene.list$tfs) > 0){
+
+                solve(trena, target.gene, gene.list$tfs, extraArgs = extraArgs)}
+
+            else{NULL}
+
+
+        }
+        else{NULL}
+}
+    # Stop the cluster
+    stopCluster(cl)
+
+    # Name the list after the genes supplied
+    names(full.result.list) <- names(gene.list)
+    return(full.result.list)
+
+}
