239f941ed83a4de0dcbf247b4bd5b38f1040986e
CoryFunk <cfunk@systemsbiology.org>
2017-04-20 17:12:19 -0700

    Update chunk.R

--- chunk.R
+++ chunk.R
@@ -4,6 +4,11 @@ library(RPostgreSQL)
 source("/scratch/github/TReNA/inst/utils/createGenomeScaleModel.R")
 source("/scratch/github/TReNA/inst/utils/coryGenomeScaleModel.R")
 
+args <- commandArgs()
+start <- as.integer(args[4])
+stop <- as.integer(args[5])
+name <- args[6]
+
 driver <- PostgreSQL()
 host <- "localhost"
 dbname <- "hg38"
@@ -11,22 +16,21 @@ user <- "trena"
 password <- "trena"
 genome.db <- dbConnect(driver, host=host, dbname=dbname, user=user, password=password)
 
-gtex.pc.fib2 <- readRDS("/scratch/data/gtex.pc.fib2")
-#fp.10 <- readRDS("/scratch/data/fp.10.rds")
+gtex.pc.fib <- readRDS("/scratch/data/gtex.pc.fib.rds")
 
-list.1 <- head(rownames(gtex.pc.fib2), 1000)
-system.time(fp.10 <- stinkyFeet(gtex.pc.fib2, list.1, 
-   "postgres://localhost/hg38", 
-   "postgres://localhost/skin_hint", 
-   5000, 
-   5000, 
+list.1 <- rownames(gtex.pc.fib[start:stop,])
+system.time(fp <- stinkyFeet(gtex.pc.fib, list.1,
+   "postgres://localhost/hg38",
+   "postgres://localhost/skin_hint",
+   5000,
+   5000,
    num.cores = 64,
    extraArgs = list("solver.list"=c("lasso", "ridge", "randomForest", "sqrtlasso", "lassopv", "pearson", "spearman"),
    "sqrtlasso"=list(num.cores=1))))
 
-system.time(test.fib.trn <- createSpecialModel(gtex.pc.fib2, fp.10, 
+system.time(test.fib.trn <- createSpecialModel(gtex.pc.fib, fp,
  num.cores = 20,
  extraArgs = list("solver.list"=c("lasso", "ridge", "randomForest", "sqrtlasso", "lassopv", "pearson", "spearman"),
  "sqrtlasso"=list(num.cores=5))))
 
-saveRDS(test.fib.trn, "test.fib.trn.rds")
+saveRDS(test.fib.trn, name)
