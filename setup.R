17a8e7306933096888f39eaf39f7c0915ef173c4
CoryFunk <cfunk@systemsbiology.org>
2017-05-08 12:12:53 -0700

    Update setup.R

--- setup.R
+++ setup.R
@@ -1,3 +1,3 @@
 source("https://bioconductor.org/biocLite.R")
 library(BiocInstaller)
-biocLite(c("glmnet", "GenomicRanges", "RSQLite", "lassopv", "randomForest", "flare", "vbsr", "foreach", "doParallel", "RPostgreSQL", "RMySQL", "DBI", "RUnit", "BiocParallel", "org.Hs.eg.db", "Biostrings", "rtracklayer"), ask=FALSE)
+biocLite(c("glmnet", "GenomicRanges", "RSQLite", "lassopv", "randomForest", "flare", "vbsr", "foreach", "doParallel", "RPostgreSQL", "RMySQL", "DBI", "RUnit", "BiocParallel", "org.Hs.eg.db", "Biostrings", "rtracklayer", "dplyr"), ask=FALSE)
