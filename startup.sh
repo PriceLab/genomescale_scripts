628701d7897f2ffa5d152e7f79a8a8af0d00a672
CoryFunk <cfunk@systemsbiology.org>
2017-05-15 11:09:47 -0700

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,12 +1,12 @@
 #/bin/bash
 
 # mount ephemeral drive
-sudo -s
+#sudo -s
 
-sudo mkfs.ext4 /dev/xvdba
-sudo mkdir -m 000 /scratch 
-echo "/dev/xvdba /scratch auto noatime 0 0" | sudo tee -a /etc/fstab
-sudo mount /scratch
+#sudo mkfs.ext4 /dev/xvdba
+#sudo mkdir -m 000 /scratch 
+#echo "/dev/xvdba /scratch auto noatime 0 0" | sudo tee -a /etc/fstab
+#sudo mount /scratch
 
 # istall latest version of R
 #echo "deb http://ftp.osuosl.org/pub/cran/bin/linux/ubuntu trusty/" >> /etc/apt/sources.list
@@ -23,27 +23,32 @@ Rscript setup.R
 mkdir -p /scratch/data
 cd /scratch/data
 #aws s3 cp s3://cory-temp/gtex.primary2.rds .
-aws s3 cp s3://cory-temp/gtex.fib.RData .
+#aws s3 cp s3://cory-temp/gtex.fib.RData .
 #aws s3 cp s3://cory-temp/first100.RDS .
-mkdir -p /scratch/data/footprints
-cd /scratch/data/footprints
-aws s3 cp s3://cory-dbtest/footprints . --recursive
+#mkdir -p /scratch/data/footprints
+#cd /scratch/data/footprints
+#aws s3 cp s3://cory-dbtest/footprints . --recursive
 
 cd /scratch
 mkdir -p /scratch/github
 cd /scratch/github/
 git clone https://github.com/PriceLab/TReNA.git
 cd /scratch/github/TReNA/
-sed -ie 's/3.4.0/3.3.0/g' /scratch/github/TReNA/DESCRIPTION
+git checkout 2b748798c5363c37f5a4db948c8078e1b60a34a4
+# change the R version requirement
+#sed -ie 's/3.4.0/3.3.0/g' /scratch/github/TReNA/DESCRIPTION
 R CMD INSTALL .
 aws s3 cp s3://cory-temp/coryGenomeScaleModel.R /scratch/github/TReNA/inst/utils/
 
 mkdir -p /scratch/db
 cd /scratch/db
-aws s3 cp s3://cory-dbtest/skin_hint.dump .
+#aws s3 cp s3://cory-dbtest/skin_hint.dump .
 aws s3 cp s3://cory-dbtest/hg38.dump .
-aws s3 cp s3://cory-dbtest/fimo.dump .
-aws s3 cp s3://cory-dbtest/03-28-2017-at-15-52-43_brain_hint.dump .
+#aws s3 cp s3://cory-dbtest/fimo.dump .
+aws s3 cp s3://cory-dbtest/brain_hint_16.dump .
+aws s3 cp s3://cory-dbtest/brain_hint_20.dump .
+aws s3 cp s3://cory-dbtest/brain_wellington_16.dump .
+aws s3 cp s3://cory-dbtest/brain_wellington_20.dump .
 
 # change the default location for postgres database storage
 # stoping postgres and changing the default directory to the scratch
@@ -65,23 +70,29 @@ sudo /etc/init.d/postgresql start
 cd /scratch/db
 sudo -u postgres psql postgres << EOF
 CREATE ROLE root WITH SUPERUSER CREATEDB CREATEROLE LOGIN ENCRYPTED PASSWORD 'trena';
-CREATE DATABASE skin_hint;
+CREATE DATABASE brain_hint_16;
+CREATE DATABASE brain_hint_20;
+CREATE DATABASE brain_wellington_16;
+CREATE DATABASE brain_wellington_20;
 CREATE DATABASE hg38;
-CREATE DATABASE fimo;
-CREATE DATABASE brain_hint;
 EOF
 
-sudo pg_restore --verbose --clean --no-acl --no-owner --dbname=skin_hint --create skin_hint.dump &
 sudo pg_restore --verbose --clean --no-acl --no-owner --dbname=hg38 --create hg38.dump &
-sudo pg_restore --verbose --clean --no-acl --no-owner --dbname=fimo --create fimo.dump &
-sudo pg_restore --verbose --clean --no-acl --no-owner --dbname=brain_hint --create 03-28-2017-at-15-52-43_brain_hint.dump &
+sudo pg_restore --verbose --clean --no-acl --no-owner --dbname=brain_hint_16 --create brain_hint_16.dump &
+sudo pg_restore --verbose --clean --no-acl --no-owner --dbname=brain_hint_20 --create brain_hint_20.dump &
+sudo pg_restore --verbose --clean --no-acl --no-owner --dbname=brain_wellington_16 --create brain_wellington_16.dump &
+sudo pg_restore --verbose --clean --no-acl --no-owner --dbname=brain_wellington_20 --create brain_wellington_20.dump &
+#sudo pg_restore --verbose --clean --no-acl --no-owner --dbname=fimo --create fimo.dump &
+
 wait
 
 cd /scratch/db
 sudo -u postgres psql postgres << EOF
 CREATE ROLE trena WITH SUPERUSER CREATEDB CREATEROLE LOGIN ENCRYPTED PASSWORD 'trena';
-grant all privileges on database skin_hint to trena;
+grant all privileges on database brain_hint_16 to trena;
+grant all privileges on database brain_hint_20 to trena;
+grant all privileges on database brain_wellington_16 to trena;
+grant all privileges on database brain_wellington_20 to trena;
 grant all privileges on database hg38 to trena;
-grant all privileges on database fimo to trena;
-grant all privileges on database brain_hint to trena;
+#grant all privileges on database fimo to trena;
 EOF
