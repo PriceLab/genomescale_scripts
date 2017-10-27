b7c681ee2335b30f9728d36fba707159e79ce53e
CoryFunk <cfunk@systemsbiology.org>
2017-10-27 15:52:04 -0700

    Update setup.R

--- setup.R
+++ setup.R
@@ -1,3 +1,3 @@
 source("https://bioconductor.org/biocLite.R")
 library(BiocInstaller)
-biocLite(c("glmnet", "GenomicRanges", "RSQLite", "lassopv", "randomForest", "flare", "vbsr", "foreach", "doParallel", "RPostgreSQL", "RMySQL", "DBI", "RUnit", "BiocParallel", "org.Hs.eg.db", "Biostrings", "rtracklayer", "dplyr", "BSgenome.Hsapiens.UCSC.hg38", "org.Hs.eg.db" "biomaRT", "AnnotationDbi"), ask=FALSE)
+biocLite(c("glmnet", "GenomicRanges", "RSQLite", "lassopv", "randomForest", "flare", "vbsr", "foreach", "doParallel", "RPostgreSQL", "RMySQL", "DBI", "RUnit", "BiocParallel", "org.Hs.eg.db", "Biostrings", "rtracklayer", "dplyr", "BSgenome.Hsapiens.UCSC.hg38", "org.Hs.eg.db" "biomaRT", "AnnotationDbi", "MotifDb", "stringr", "BSgenome.Hsapiens.UCSC.hg19", "BSgenome.Mmusculus.UCSC.mm10", "SNPlocs.Hsapiens.dbSNP150.GRCh38"), ask=FALSE)
