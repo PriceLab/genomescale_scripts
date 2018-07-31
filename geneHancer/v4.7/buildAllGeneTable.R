8886c89a1930bfb861550b91c0fac0c9724725b7
paul-shannon <paul.thurmond.shannon@gmail.com>
2018-07-31 13:08:15 -0700

    now operates on all 45k genes (by geneSymbol)

--- geneHancer/v4.7/buildAllGeneTable.R
+++ geneHancer/v4.7/buildAllGeneTable.R
@@ -5,6 +5,8 @@ library(org.Hs.eg.db)
 library(BiocParallel)
 tbl.chrom <- as.data.frame(org.Hs.egCHR)
 genes.chrom2 <- subset(tbl.chrom, chromosome=="2")$gene_id   # 4245
+genes.all <- unique(tbl.chrom$gene_id)
+length(genes.all) # 59973
 keytypes(org.Hs.eg.db)
 #  [1] "ACCNUM"       "ALIAS"        "ENSEMBL"      "ENSEMBLPROT"  "ENSEMBLTRANS" "ENTREZID"
 #  [7] "ENZYME"       "EVIDENCE"     "EVIDENCEALL"  "GENENAME"     "GO"           "GOALL"
@@ -12,8 +14,18 @@ keytypes(org.Hs.eg.db)
 # [19] "PFAM"         "PMID"         "PROSITE"      "REFSEQ"       "SYMBOL"       "UCSCKG"
 # [25] "UNIGENE"      "UNIPROT"
 geneSyms.chrom2 <- select(org.Hs.eg.db, keys=genes.chrom2, keytype="ENTREZID", columns="SYMBOL")$SYMBOL  # 4245
+
 length(geneSyms.chrom2)
 x <- bplapply(geneSyms.chrom2, getAllEnhancers)
+
+geneSyms.all <- unique(select(org.Hs.eg.db, keys=genes.all, keytype="ENTREZID", columns="SYMBOL")$SYMBOL)
+length(geneSyms.all)  # 59905
+x <- bplapply(geneSyms.all, getAllEnhancers)
+length(x) # 59905
+tbl.enhancers <- do.call(rbind, x)
+length(unique(tbl.enhancers$geneSymbol)) # [1] 45920
+save(tbl.enhancers, file="tbl.enhancers.allGenes.RData")
+
 tbl.enhancers.chr2 <- do.call(rbind, x)
 dim(tbl.enhancers.chr2)
 length(unique(tbl.enhancers.chr2$geneSymbol))
